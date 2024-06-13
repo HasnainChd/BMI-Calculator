4636575b9b0cdba8b3234b641506fc202afa5adb
Muhammad <hasnainyousafzai8566@gmail.com>
2024-06-13 21:25:08 +0500

    initial commit

---
+++ lib/bmi_screen.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+
+class BmiScreen extends StatefulWidget {
+  const BmiScreen({super.key});
+
+  @override
+  State<BmiScreen> createState() => _BmiScreenState();
+}
+
+class _BmiScreenState extends State<BmiScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
